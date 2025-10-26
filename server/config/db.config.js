fd5ca53dce80dbc7d26e5822ae9a388d1819464e
Takunda <kabvunyet@gmail.com>
2025-10-26 09:14:50 +0200

    Fix: added pg and dialectModule

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,16 +1,15 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_URL, {
-  dialect: "postgres",
-  protocol: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // Supabase requires this
-    },
-  },
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    dialectModule: require("pg"),
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
