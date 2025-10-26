430204b8f81882743891870902a34e12ebf84b8d
Takunda <kabvunyet@gmail.com>
2025-10-26 11:22:33 +0200

    fix: refactor database configuration to use DB_URL and streamline Sequelize initialization

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,15 +1,17 @@
 const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    dialectModule: require("pg"),
-    logging: false,
-  }
-);
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // for Supabase SSL
+    },
+  },
+  logging: false,
+});
 
 module.exports = sequelize;
