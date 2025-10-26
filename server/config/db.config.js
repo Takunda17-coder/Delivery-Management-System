09a0b6471eeafe13037076e0c32af19faaf705ee
Takunda <kabvunyet@gmail.com>
2025-10-26 11:25:02 +0200

    fix: update Sequelize initialization to use DB_NAME, DB_USER, and DB_PASSWORD for improved configuration

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,17 +1,21 @@
 const { Sequelize } = require("sequelize");
-const dotenv = require("dotenv");
 
-dotenv.config();
-
-const sequelize = new Sequelize(process.env.DB_URL, {
-  dialect: "postgres",
-  dialectOptions: {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    dialectModule: require("pg"),
+    dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false, // for Supabase SSL
     },
   },
-  logging: false,
-});
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
