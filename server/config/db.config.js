50f4443f7f56d2b77119250b1e9e87e497f7fcba
Takunda <kabvunyet@gmail.com>
2025-10-27 14:36:02 +0200

    fix: remove SUPABASE_DB_HOST from Sequelize configuration and update .env file

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -6,8 +6,6 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: { require: true, rejectUnauthorized: false },
   },
-  host: process.env.SUPABASE_DB_HOST,
-  port: 6543,
 });
 
 module.exports = sequelize;
