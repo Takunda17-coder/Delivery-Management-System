79aca4c3bcf039e1931038c6d5efc2f40eed7442
Takunda <kabvunyet@gmail.com>
2025-10-27 15:47:55 +0200

    fix: update DATABASE_URL in .env file to use the correct port and format db.config.js for consistency

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -2,10 +2,13 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
+  dialect: 'postgres',
+  logging: false,
   dialectOptions: {
-    ssl: { require: true, rejectUnauthorized: false },
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
 });
-
 module.exports = sequelize;
