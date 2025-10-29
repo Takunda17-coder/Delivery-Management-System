3d9db74a345892102f945456d33289f8bb045c5b
Takunda <kabvunyet@gmail.com>
2025-10-29 07:41:55 +0200

    fix: add pg dependency to db.config.js for PostgreSQL dialect support

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -3,11 +3,13 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 require("dotenv").config();
+const pg = require("pg");
 
 const caCert = fs.readFileSync(path.resolve("./config/prod-ca-2021.crt"), "utf8");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
