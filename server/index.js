dabae2e745917c551a25df096a4b1687b5c0755f
Takunda <kabvunyet@gmail.com>
2025-11-06 21:39:15 +0200

    Add allowedHeaders to CORS configuration for improved request handling

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ app.use(
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
